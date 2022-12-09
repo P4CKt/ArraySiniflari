83062cc02fedcd83e09496440cb427b477101b47
P4CKt <116915510+P4CKt@users.noreply.github.com>
2022-12-09 16:23:04 +0300

    Add files via upload
    www.patika.dev

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace odev_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sort sıralama
+            int[] sayidizisi = {23,12,4,86,72,3,11,17,};
+            foreach (var sayi in sayidizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            Console.WriteLine("******Sıralı Dizi********");
+            Array.Sort(sayidizisi);
+            foreach (var sayi in sayidizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            // Clear 
+             Console.WriteLine("******Clear********");
+            Array.Clear(sayidizisi,2,3); // 2. indexten başlayarak 3 tane değeri sıfırladı
+            foreach (var sayi in sayidizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            Console.WriteLine("******Reverse********");
+            //Reverse ters çeviri
+            Array.Reverse(sayidizisi);
+            foreach (var sayi in sayidizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            // index of (aranan değerin sırasını söyler)
+            Console.WriteLine("******indexOf********");
+            Console.WriteLine(Array.IndexOf(sayidizisi,23));
+            
+
+
+        }
+    }
+}
